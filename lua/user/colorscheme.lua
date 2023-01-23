a1e3089f2596e1476724ec31b244b5919a22dc8d
Pawel Guspiel <pawel.guspiel@ocado.com>
2023-01-23 21:56:11 +0100

    colorscheme config

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,9 @@
-local colorscheme = "tokyonight"
+-- local colorscheme = "tokyonight"
+local colorscheme = "catppuccin-mocha"
+-- local colorscheme = "darkplus"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
+  vim.notify("can't load " .. colorscheme)
   return
 end
